cbcea517d706a50154aa5a0850d60d2155a695e5
EmmanuelSHYIRAMBERE <emashyirambere1@gmail.com>
2026-05-14 11:53:01 +0200

    chore(mod9): add httpx, tenacity, django-cors-headers to pre-commit mypy deps

---
+++ projects/python-url-shortener-microservice/url_preview/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class UrlPreviewConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "url_preview"
+    verbose_name = "URL Preview Service"
